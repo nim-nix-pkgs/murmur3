6b9d140376f7c0f17c85497c14d40fae21a5dd69
hugosenari <hugosenari@gmail.com>
2022-03-23 22:40:45 -0300

    feat(nimpkgs): remove lock

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''A simple MurmurHash3 wrapper for Nim'';
+  description = ''Simple Murmurhash3 wrapper (for strings only at the moment). MurmurHash code is by Austin Appleby and in the public domain.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-murmur3-v0_1_0.flake = false;
-  inputs.src-murmur3-v0_1_0.ref   = "refs/tags/v0.1.0";
-  inputs.src-murmur3-v0_1_0.owner = "boydgreenfield";
-  inputs.src-murmur3-v0_1_0.repo  = "nimrod-murmur";
-  inputs.src-murmur3-v0_1_0.type  = "github";
+  inputs.src-murmur-v0_1_0.flake = false;
+  inputs.src-murmur-v0_1_0.ref   = "refs/tags/v0.1.0";
+  inputs.src-murmur-v0_1_0.owner = "boydgreenfield";
+  inputs.src-murmur-v0_1_0.repo  = "nimrod-murmur";
+  inputs.src-murmur-v0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-murmur3-v0_1_0"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-murmur-v0_1_0"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-murmur3-v0_1_0";
+    src  = deps."src-murmur-v0_1_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
